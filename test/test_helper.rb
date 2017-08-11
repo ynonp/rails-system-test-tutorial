3c7e8391d08e2c0ebe8f327d870f321f8df3a01d
ynonp <ynonperek@gmail.com>
2017-08-11 16:44:40 +0300

    driver registrations

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,16 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "selenium/webdriver"
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
