5fe9b47761d79543d81a46ba1d3b7fa0ca53c7bf
ynonp <ynonperek@gmail.com>
2017-08-11 15:40:39 +0300

    fixed test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-require 'capybara/rails'
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
